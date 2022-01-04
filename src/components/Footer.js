809611624aa5ae6f95b380543c226cbfdc0cc4a3
drodrigues21 <davidvilar21@gmail.com>
2022-01-04 18:50:29 +0900

    added desktop version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import './Footer.css';
 
 export default function Footer() {
     return (
-        <div className="attribution">
+        <footer className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
             Coded by <a href="https://github.com/drodrigues21" target="_blank" rel="noreferrer">David Rodrigues</a>.
-        </div>
+        </footer>
     )
 }
